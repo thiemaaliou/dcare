47bfd2d6bb750bb2b73b5b6b1001e235c4dbe2f0
Madjiguene <niang.madjiguene.94@gmail.com>
2020-10-01 14:09:46 +0000

    correction env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://192.168.1.45:8080/api/',
+  appUrl: 'localhost:4200/',
+  adminAppUrl:'https://online.secusociale.sn/cssipres/',
+  appName: 'IPRES-CSS',
+  appHost: 'www.ipres-css.sn'
 };
