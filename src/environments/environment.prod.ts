dd18d30cbbaac58a925b6013e2cdcb8d1fde0ebc
Mamadou SY <symamadou045@gmail.com>
2020-11-07 11:54:35 +0000

    corrections textes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   appUrl: 'https://online.secusociale.sn/',
   adminAppUrl: 'https://online.secusociale.sn/cssipres/',
   appName: 'CSS-IPRES',
-  appHost: 'www.css-ipres.sn',
+  appHost: 'www.online.secusociale.sn',
   siteCapchatKey: '6LfEKdgZAAAAAFQjO5GMof_Da0VCBv9LBTblsuqz',
   siteSecretKey: '6LfEKdgZAAAAAAsZTmssEZwxNbW5iGwNRIO7D2-z',
   footerText: 'Copyright © CSS-IPRES'
